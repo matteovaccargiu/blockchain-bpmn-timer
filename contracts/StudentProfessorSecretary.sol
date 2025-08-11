e4ae297ae39b0bf3c0139808b51da3794558792d
matteovaccargiu <124043561+matteovaccargiu@users.noreply.github.com>
2025-08-11 23:12:56 +0200

    Update StudentProfessorSecretary.sol

--- contracts/StudentProfessorSecretary.sol
+++ contracts/StudentProfessorSecretary.sol
@@ -38,9 +38,9 @@ contract StudentProfessorSecretary is ReentrancyGuard, Ownable, Pausable {
     mapping(string => GatewayData) public gatewayMap;
 
     constructor() Ownable(msg.sender) Pausable() {
-        participantAddresses["Professor"] = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
-        participantAddresses["Student"] = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
-        participantAddresses["Secretary"] = 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db;
+        participantAddresses["Professor"] = ;
+        participantAddresses["Student"] = ;
+        participantAddresses["Secretary"] = ;
 
         elementStates["StartEvent_Professor"] = State.ENABLED;
 
